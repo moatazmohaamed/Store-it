d4fc16567f8d7d57577d1fd641e8689329db20df
Moataz Mohamed <moataz9898k@gmail.com>
2025-06-11 06:19:45 +0300

    Server settings modified

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,9 +1,9 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [],
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
